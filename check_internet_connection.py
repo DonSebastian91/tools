712f1eb6f986de566da829a1ab34ffd65ce6c691
Sebastian Lehner <lehner@wiwi.uni-wuppertal.de>
2017-06-25 00:37:41 +0200

    [FIX]

--- check_internet_connection.py
+++ check_internet_connection.py
@@ -1,8 +1,7 @@
 import requests
 
-req = requests.get('http://www.google.de')
-
 while True:
+	req = requests.get('http://www.google.de')
 	if req.status_code == 200:
 		print('**********************************')
 		print('* Internet Connection available. *')
